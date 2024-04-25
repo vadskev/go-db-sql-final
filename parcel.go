3d809d9a24431833ee97f5217a1a9bbb15595c49
Vadim Vaskevich <vadskev@ya.ru>
2024-04-25 12:11:15 +0300

    feat: add SetStatus()

--- parcel.go
+++ parcel.go
@@ -87,6 +87,12 @@ func (s ParcelStore) GetByClient(client int) ([]Parcel, error) {
 
 func (s ParcelStore) SetStatus(number int, status string) error {
 	// реализуйте обновление статуса в таблице parcel
+	_, err := s.db.Exec("UPDATE parcel SET status = :status WHERE number = :number",
+		sql.Named("number", number),
+		sql.Named("status", status))
+	if err != nil {
+		return err
+	}
 
 	return nil
 }
